765fb4d2f8fa56631aba4279ab42f4ffb9b0105d
Juanma <jmlpdr.93@gmail.com>
2020-08-07 18:13:23 +0100

    :lock: Enable securityConfig API

--- src/main/java/com/juanmanuel/cardatabase/SecurityConfig.java
+++ src/main/java/com/juanmanuel/cardatabase/SecurityConfig.java
@@ -31,15 +31,15 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         // Con esto permitimos el acceso total al REST sin necesidad de autenticación
-        http.csrf().disable().cors().and().authorizeRequests().anyRequest().permitAll();
-        /*http.csrf().disable().cors().and().authorizeRequests()
+        /*http.csrf().disable().cors().and().authorizeRequests().anyRequest().permitAll();*/
+        http.csrf().disable().cors().and().authorizeRequests()
                 .antMatchers(HttpMethod.POST, "/login").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 // Filter for the api/login requests
                 .addFilterBefore(new LoginFilter("/login", authenticationManager()), UsernamePasswordAuthenticationFilter.class)
                 // Filter for the other requests to check JWT in header
-                .addFilterBefore(new AuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);*/
+                .addFilterBefore(new AuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
     }
 
     @Bean
